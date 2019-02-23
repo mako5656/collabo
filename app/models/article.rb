0336b39c59618ce444eb56ef4be925b65e3b4d9e
ikamimakoto <makoto09142425@gmail.com>
2019-02-23 21:36:56 +0000

    ‘IkamiDiary’

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,8 @@
 class Article < ApplicationRecord
 	attachment :image
+	<% =begin %>
+	validates :title, presence: true
+	validates :weather, presence: true
+	validates :body, presence: true
+	<% =end %>
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
   get 'posts/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/top' => 'homes#top'
-  post '/posts' => 'posts#create'
   get '/new' => 'posts#new'
+
   get '/posts' => 'posts#index'
+  post '/posts' => 'posts#create'
   get '/posts/:id' => 'posts#show', as: 'post'
   get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
   patch '/posts/:id' => 'posts#update', as: 'update_post'
